393e2a1e44fa69d77ee940775950ea360a6d17ae
Seurimas <nicholasanderson5@gmail.com>
2017-07-29 01:42:52 -0500

    Messy messy messy. Late late late.

--- src/World/Casting.elm
+++ src/World/Casting.elm
@@ -4,11 +4,39 @@ import World exposing (..)
 import Slime exposing (..)
 import World.Components exposing (..)
 import World.Input as Input
+import Vector2 exposing (..)
+
 
 playerCasting : Float -> World -> World
 playerCasting delta world =
     let
-        target e = Input.gameMouse world
-        shoot ({b}) world =
-            let
-                playerCenter = center b
+        players =
+            world &. (entities2 player transforms)
+
+        target =
+            Input.gameMouse world
+
+        spawn pos speed owner =
+            { pos = pos
+            , vel = scale speed (directionFromTo pos target)
+            , owner = owner
+            }
+
+        shoot { a, b, id } world =
+            if wantToShoot a then
+                let
+                    playerCenter =
+                        center b
+
+                    ( _, updatedWorld ) =
+                        forNewEntity
+                            &=> ( playerProjectiles, spawn playerCenter id )
+                in
+                    updatedWorld
+            else
+                world
+
+        updatedWorld =
+            List.foldl shoot world players
+    in
+        updatedWorld
